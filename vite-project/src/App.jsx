0f12f8dad3173d6807ed88c6cf9bedba4291c097
hiroyuki718 <hiroyuki.tamura@gmail.com>
2024-02-04 15:41:45 -0500

    homework

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,36 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {puppyList} from './data.js'
-
+import React, { useState } from 'react';
+import './App.css';
+import { puppyList } from './data.js';
+import './index.css'
 function App() {
-  const [count, setCount] = useState(0)
+  const [featPupId, setFeatPupId] = useState(null);
+  const [puppies, setPuppies] = useState(puppyList);
+
+  function handleClick(puppyId) {
+    setFeatPupId(puppyId);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="puppy-list">
+        {puppies.map((puppy) => (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 3)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn test
-      </p>
-    </>
-  )
+
+      {featPupId && (
+        <div className="featured-puppy">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
